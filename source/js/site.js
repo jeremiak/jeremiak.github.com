a9c6c1e938b31805196e5ee7c9d7c05d8ca5ff53
Jeremia Kimelman <jbkimelman@gmail.com>
2015-03-19 19:16:03 +0700

    new js for altering jobs w/ encapd logic

--- source/js/site.js
+++ source/js/site.js
@@ -1,38 +1,41 @@
+function nextJobHandler (jobSelector) {
+  var currentJobIndex = 0,
+      totalJobs = document.querySelectorAll(jobSelector).length, 
+      moveBy = -2, moveByUnit = 'em';
+
+  var marginTopText = function (margin) {
+    return (margin * moveBy) + moveByUnit;
+  }
+
+  var nextMarginTop = function () {
+    if (currentJobIndex == totalJobs -1) {
+      currentJobIndex = 0;
+      return marginTopText(0);
+    }
+    currentJobIndex += 1;
+    return marginTopText(currentJobIndex);
+  } 
+
+  var nextJob = function () {
+    document.querySelector(jobSelector).style.marginTop = nextMarginTop();
+  }
+
+  return nextJob; 
+}
+
+function showColophon(e) {
+  var colophonElement = e.target;
+  colophonElement.classList.add('hide');
+}
+
 window.onload = function (){
 
   console.log('nice to see you, old friend :)');
 
-  function changeJobTitle() {
-    var jobElement = document.getElementsByClassName('job')[0].children[0];
+  document.getElementById('colophon').addEventListener('click', showColophon);
 
-    if (currentJobIndex < jobTitles.length - 1) {
-      currentJobIndex += 1;
-    }
-    else {
-      currentJobIndex = 0;
-    }
-
-    jobElement.textContent = jobTitles[currentJobIndex];
-  }
-
-  function showColophon(e) {
-    console.log('yo');
-    var colophonElement = e.target;
-    colophonElement.classList.add('hide');
-  }
-
-  var x = document.getElementById('colophon').addEventListener('click', showColophon);
-
+  var nextJob = new nextJobHandler('.job');
   if (window.location.pathname.indexOf('projects') == -1) {
-    var currentJobIndex = -1, jobTitles = [
-      'a front end developer',
-      'a developer advocate',
-      'an open data evangelist',
-      'a hacker',
-      'a 2014 code for america fellow'
-    ];
-
-    changeJobTitle();
-    setInterval(changeJobTitle, 1700);
+    setInterval(nextJob, 2500);
   }
 }
