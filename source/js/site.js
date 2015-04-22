2629e273c793eaacc48d9f7bfc8c9b69e1b57476
Jeremia Kimelman <jbkimelman@gmail.com>
2015-04-22 19:12:37 -0400

    when ribbon is clicked, trigger animation

--- source/js/site.js
+++ source/js/site.js
@@ -1,3 +1,12 @@
 window.onload = function (){
   console.log('nice to see you, old friend :)');
 }
+
+document.addEventListener('click', function(e) {
+  var bodyClasses = document.querySelector('body').classList,
+      mouseX = e.clientX,
+      mouseY = e.clientY;
+  if (mouseY < 15) {
+    bodyClasses.add('new');
+  }
+});
