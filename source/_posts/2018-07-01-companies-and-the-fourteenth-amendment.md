3894fefce26c78f4a527416b89a3961566349452
Jeremia Kimelman <jbkimelman@gmail.com>
2018-07-13 13:51:38 +0200

    Better scrollytelling on 14th amendment post

--- source/_posts/2018-07-01-companies-and-the-fourteenth-amendment.md
+++ source/_posts/2018-07-01-companies-and-the-fourteenth-amendment.md
@@ -2,61 +2,66 @@
 title: Companies and the 14th amendment
 ---
 
-Even before this last week of horrendous news from the Supreme Court, I've been thinking about the institution quite a bit recently because I am reading "We the Corporations: How American businesses won their civil rights" by Adam Winkler. He chronicles the history of the ever expanding rights granted to corporations by the Supreme Court. Though the recent (and _very shitty_) Citizens United decision gives corporations the right to freely spend on domestic political elections, companies have been exploiting the 14th Amendment since nearly its ratification.
+Even before these last few weeks of horrendous news from the Supreme Court, I've been thinking about the institution quite a bit recently because I am reading "We the Corporations: How American businesses won their civil rights" by Adam Winkler. He chronicles the history of the ever expanding rights granted to corporations by the Supreme Court. Though the recent (and _very shitty_) Citizens United decision gives corporations the right to freely spend on domestic political elections, companies have been exploiting the 14th Amendment since nearly its ratification.
 
-The 14th Amendment, originally drafted to extend rights to former slaves, was passed following the Civil War. Perhaps predictably, because this is America, the land of the dollar, corporations started to use their massive bank accounts to fight for the rights of "artificial" persons. In fact, Winkler cites a startling, albeit brief, finding from a long dead law librarian of Congress named Charles Wallace Collins about the types of parties to SCOTUS cases related to the 14th amendment. Collins analyzed all the 14th amendment cases between 1872 and 1910 and found that corporations brought the overwhelming number of cases before the Court.
+The 14th Amendment, originally drafted to extend rights to former slaves, was passed following the Civil War. Perhaps predictably, because this is America: the land of the dollar, corporations started to use their massive bank accounts to fight for the rights of "artificial" persons. In fact, Winkler cites a startling, albeit brief, finding from a long dead law librarian of Congress named Charles Wallace Collins about the types of parties involved in cases related to the 14th amendment. Collins analyzed all the 14th amendment cases between 1872 and 1910 and found that corporations brought the overwhelming number of cases before the Court.
 
 <style>
+  .container {
+    position: relative;
+  }
+
   .chart-container {
-    border: 1px solid red;
     position: sticky;
-    top: 1rem;
+    top: 3.5rem;
   }
 
   .step {
-    border: 1px solid black;
     min-height: 20rem;
+    opacity: .2;
+    transition: opacity 1s linear;
   }
 
   .step p {
     position: sticky;
-    top: 1rem;
+    top: .5rem;
   }
 </style>
 
 <div class="container clearfix">
-  <div class="col col-6 steps">
+  <div class="chart-container no-js" id="outages-differences">
+    <p class="no-js-msg">This article requires Javascript to generate charts.</p>
+    <div class="chart"></div>
+    <div class="caption mono"></div>
+    {% include chart_source.html href="https://www.google.com/url?q=https://books.google.com/books?id%3DtBBD9JdqFNkC%26pg%3DPA138%23v%3Donepage%26q%26f%3Dfalse&sa=D&ust=1530321642501000&usg=AFQjCNFgKM2vG5F5Z-IZGHJEAxpw8RDRaw" source="The Fourteenth Amendment and the States: A Study of the Operation of the Restraint Clauses of Section One of the Fourteenth Amendment to the Constitution of the United States by Charles Wallace Collins" %}
+  </div>
+
+  <div class="steps">
     <div class="step" data-line="total-opinions">
-      <p>
+      <p class="bg-gold-translucent px1 py2">
       Between 1872 and 1910 there were a total of 604 cases that pivoted on the 14th amendment.
       </p>
     </div>
     <div class="step" data-line="african-americans">
-      <p>
+      <p class="bg-gold-translucent px1 py2">
         Just 28 of them were brought on behalf of African Americans. So who brought the rest?
       </p>
     </div>
     <div class="step" data-line="individuals">
-      <p>
+      <p class="bg-gold-translucent px1 py2">
         Well, the 14th amendment was used by individuals, just not Black people. 264 of the total cases were brought by individuals seeking redress against the government.
       </p>
     </div>
     <div class="step" data-line="corporations">
-      <p>
+      <p class="bg-gold-translucent px1 py2">
         Companies, with their large piles of gold and army of lawyers, brought 312 cases before the Supreme Court under the 14th Amendment. More than half of _all_ the cases looked at in Collins' survey.
       </p>
     </div>
   </div>
 
-  <div class="chart-container col col-6 no-js" id="outages-differences">
-    <p class="no-js-msg">This article requires Javascript to generate charts.</p>
-    <div class="chart"></div>
-    <div class="caption mono"></div>
-    <div class="mono">
-      {% include chart_source.html href="https://www.google.com/url?q=https://books.google.com/books?id%3DtBBD9JdqFNkC%26pg%3DPA138%23v%3Donepage%26q%26f%3Dfalse&sa=D&ust=1530321642501000&usg=AFQjCNFgKM2vG5F5Z-IZGHJEAxpw8RDRaw" source="The Fourteenth Amendment and the States: A Study of the Operation of the Restraint Clauses of Section One of the Fourteenth Amendment to the Constitution of the United States by Charles Wallace Collins" %}
-    </div>
-  </div>
 </div>
+
+All of this was pretty surprising to me because I was under the (naive) impression that Citizens United was somehow unique in its corporate abuse of an amendment written to explicitly grant rights to _real_ humans. But clearly, this has been going on for a long time.
 
 This just goes to show you that the more you dig into the history of this country, the more you'll find the compounded evils of corporate greed and racism. Or, at least, that's frequently my conclusion.
 
--- source/js/posts/companies-and-the-fourteenth-amendment.js
+++ source/js/posts/companies-and-the-fourteenth-amendment.js
@@ -1,32 +1,32 @@
-(function(window) {
-  const height = 500;
-  const margin = { x: 100, y: 100 };
-  const scroller = scrollama();
-  const width = d3.select(".chart-container").node().clientWidth;
+;(function(window) {
+  const margin = { x: 100, y: 100 }
+  const scroller = scrollama()
+  const width = d3.select(".chart-container").node().clientWidth
+  const height = width > 700 ? 500 : 340
 
-  d3.selectAll(".no-js").classed("no-js", false);
+  d3.selectAll(".no-js").classed("no-js", false)
 
   const $svg = d3
     .select(".chart")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
 
   d3.csv("/data/parties-to-fourteenth-amendment-cases-1872-1910.csv")
     .then(data => {
-      const years = [];
-      const opinions = [];
+      const years = []
+      const opinions = []
       data.forEach(d => {
-        years.push(+d["October Term"]);
-        opinions.push(+d["Total opinions"]);
-      });
+        years.push(+d["October Term"])
+        opinions.push(+d["Total opinions"])
+      })
 
       const colors = {
         "total-opinions": "black",
         "african-americans": "black",
         individuals: "black",
         corporations: "black"
-      };
+      }
 
       const lines = [
         "Total opinions",
@@ -36,58 +36,56 @@
       ].map(key => ({
         key,
         values: data.map((d, i) => {
-          const other = data.slice(0, i + 1);
-          const year = +d["October Term"];
-          console.log(key, year, i, other);
-          const count = +d[key];
-          const total = d3.sum(other, dd => +dd[key]);
-          return { count, total, year };
+          const other = data.slice(0, i + 1)
+          const year = +d["October Term"]
+          console.log(key, year, i, other)
+          const count = +d[key]
+          const total = d3.sum(other, dd => +dd[key])
+          return { count, total, year }
         })
-      }));
-
-      console.log({ lines });
+      }))
 
       const total = lines
         .find(l => l.key === "Total opinions")
-        .values.map(d => d.total);
+        .values.map(d => d.total)
 
       const xScale = d3
         .scaleLinear()
         .domain(d3.extent(years))
-        .range([0 + margin.x, width - margin.x]);
+        .range([0 + margin.x, width - margin.x])
       const yScale = d3
         .scaleLinear()
         .domain(d3.extent(total))
-        .range([height - margin.y, 0 + margin.y]);
+        .range([height - margin.y, 0 + margin.y])
 
       const lineGenerator = d3
         .line()
         .x(d => xScale(d.year))
         .y(d => yScale(d.total))
-        .curve(d3.curveCatmullRom.alpha(0.5));
+        .curve(d3.curveCatmullRom.alpha(0.5))
 
       const $yAxis = $svg
         .append("g")
         .attr("class", "axis y-axis")
         .attr("transform", `translate(${margin.x * 0.75}, 0)`)
-        .call(d3.axisLeft(yScale));
+        .call(d3.axisLeft(yScale))
 
       $yAxis
         .append("text")
         .attr("transform", `rotate(270) translate(-80, -${margin.x / 2})`)
-        .text("Cases involving the 14th amendment since 1872");
+        .text("Cases involving the 14th amendment since 1872")
 
-      const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+      const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"))
       const $xAxis = $svg
         .append("g")
-        .attr("class", "axis y-axis")
+        .attr("class", "axis x-axis")
         .attr("transform", `translate(0, ${height - margin.y / 1.25})`)
-        .call(xAxis);
+        .call(xAxis)
 
       $xAxis
         .append("text")
         .attr("transform", `translate(${width / 2}, ${margin.y / 2})`)
-        .text("Year");
+        .text("Year")
 
       const $paths = $svg
         .append("g")
@@ -99,16 +97,16 @@
         .attr("id", d => kebab(d.key))
         .attr("d", d => lineGenerator(d.values))
         .attr("stroke", "none")
-        .attr("fill", "none");
+        .attr("fill", "none")
 
       function animateLine(id) {
-        const $path = d3.select(`#${id}`);
-        const pathLength = $path.node().getTotalLength();
-        const data = $path.datum();
-        const finalColor = colors[id];
-        const last = data.values.pop();
-        const textX = xScale(last.year - 5);
-        const textY = yScale(last.total + 10);
+        const $path = d3.select(`#${id}`)
+        const pathLength = $path.node().getTotalLength()
+        const data = $path.datum()
+        const finalColor = colors[id]
+        const last = data.values.pop()
+        const textX = xScale(last.year - 5)
+        const textY = yScale(last.total + 10)
 
         $path
           .attr("stroke", "gold")
@@ -116,13 +114,13 @@
           .attr("stroke-dashoffset", pathLength)
           .transition()
           .duration(2000)
-          .attr("stroke-dashoffset", 0);
+          .attr("stroke-dashoffset", 0)
 
         $path
           .transition()
           .delay(2000)
           .duration(500)
-          .attr("stroke", finalColor);
+          .attr("stroke", finalColor)
 
         $svg
           .select("#lines")
@@ -136,10 +134,10 @@
           .transition()
           .delay(2100)
           .duration(200)
-          .attr("opacity", 1);
+          .attr("opacity", 1)
       }
 
-      const animated = {};
+      const animated = {}
       scroller
         .setup({
           step: ".step",
@@ -147,16 +145,18 @@
           graphic: ".chart-container"
         })
         .onStepEnter(e => {
-          // debugger;
-          const line = e.element.dataset.line;
-          console.log({ e });
-          if (e.direction === "up") return;
-          if (animated[line]) return;
-          animateLine(line);
-          animated[line] = true;
-        });
+          const { direction, element } = e
+          const $el = d3.select(element)
+          const line = element.dataset.line
+          if (direction === "up") return
+          if (animated[line]) return
+          animateLine(line)
+          animated[line] = true
+
+          $el.style("opacity", 1)
+        })
     })
     .catch(e => {
-      console.error("Uh oh", e);
-    });
-})(window);
+      console.error("Uh oh", e)
+    })
+})(window)
